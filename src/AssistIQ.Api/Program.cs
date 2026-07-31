7cf339de2f07d00b4f7c505a8f5cee719454ad82
Lê Trọng Duy <letrongduy113@gmail.com>
2026-08-01 00:24:59 +0700

    fix: parse postgres:// URL connection string from Render in Program.cs

--- src/AssistIQ.Api/Program.cs
+++ src/AssistIQ.Api/Program.cs
@@ -51,8 +51,16 @@ builder.Services.AddHealthChecks()
     .AddDbContextCheck<AssistIQDbContext>("database");
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("postgres://"))
+{
+    var uri = new Uri(connectionString);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString = $"Host={uri.Host};Port={(uri.Port > 0 ? uri.Port : 5432)};Database={uri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SslMode=Prefer";
+}
+
 builder.Services.AddDbContext<AssistIQDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.Configure<UsageCostOptions>(builder.Configuration.GetSection("UsageCost"));
 
